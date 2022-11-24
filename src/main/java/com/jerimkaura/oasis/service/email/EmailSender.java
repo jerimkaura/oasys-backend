bd4ffcfe935b9165ca52ebbf7dfe26fc3e1cdec7
jerimkaura <jerimkaura001@gmail.com>
2022-11-24 12:47:21 +0300

    - Updated email sending to have dynamic subject

--- src/main/java/com/jerimkaura/oasis/service/email/EmailSender.java
+++ src/main/java/com/jerimkaura/oasis/service/email/EmailSender.java
@@ -1,5 +1,5 @@
 package com.jerimkaura.oasis.service.email;
 
 public interface EmailSender {
-    void sendEmail(String to, String email);
+    void sendEmail(String to, String email, String subject);
 }
